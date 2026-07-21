155f0db93d1c41db33c3bff918d542be85a1b33b
Naphezir <2milion2000000@gmail.com>
2026-07-21 17:57:30 +0200

    refactor: add default values to application settings

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str
-    app_version: str
-    app_description: str
-    debug: bool
+    app_name: str = "Pharmacy Inventory API"
+    app_version: str = "0.1.0"
+    app_description: str = "Backend API for pharmacy inventory management."
+    debug: bool = True
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', case_sensitive=False)
 
 
